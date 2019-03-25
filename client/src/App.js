666b5be77a493c97a5dc5dd7cf9fd697d5e99dea
Jensen Santos <jensen.santos20@gmail.com>
2019-03-24 23:41:05 -0600

    updating codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,124 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import home from './components/mainpage'
+
+
+import axios from 'axios'
+import Signup from './components/sign-up'
+import LoginForm from './components/login-form'
+
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//        <BrowserRouter>
+//             <div className="App">
+
+//               <Navbar/>
+//                 <Switch>
+//                     <Route exact path="/" component={Home}/>
+//                     <Route path="/cart" component={Cart}/>
+//                   </Switch>
+//              </div>
+//        </BrowserRouter>
+
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    }
+
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  updateUser(userObject) {
+    this.setState(userObject)
+  }
+
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ')
+      console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        })
+      }
+    })
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <BrowserRouter>
+        <div className="App">
+
+          <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+          <Switch>
+            {/* route for shop */}
+            <Route exact path="/" component={Home} />
+            <Route path="/cart" component={Cart} />
+            {/* route for mainpage or front page */}
+            <Route path="/Home" component={home} />
+
+            <Route
+              path="/signup"
+              render={() =>
+                <Signup
+                  signup={this.signup}
+                />}
+            />
+            <Route
+              path="/login"
+              render={() =>
+                <LoginForm
+                  updateUser={this.updateUser}
+                />}
+            />
+           
+          </Switch>
+        </div>
+      </BrowserRouter>
+
+
+
+
+
+
+
     );
   }
 }
