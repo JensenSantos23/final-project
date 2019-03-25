0b45d53f28373f12ebc66c19a4803ed84882ca9f
Jensen Santos <jensen.santos20@gmail.com>
2019-03-24 22:06:03 -0600

    Updates

---
+++ precache-manifest.be18e2a3776791ae7d641d7b4cd71543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/final-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cabc802b7bb02805ec54",
+    "url": "/final-project/static/js/runtime~main.797a4eca.js"
+  },
+  {
+    "revision": "91888118cb60e560ee7f",
+    "url": "/final-project/static/js/main.39925b80.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/final-project/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "91888118cb60e560ee7f",
+    "url": "/final-project/static/css/main.b1132641.chunk.css"
+  },
+  {
+    "revision": "51ee4cc4e6a2fb599e543d0bce1bb1e3",
+    "url": "/final-project/index.html"
+  }
+];
